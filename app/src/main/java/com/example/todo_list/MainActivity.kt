30c2cfb10f988f9de8ce13540f9522033ee886e1
Kirusha2281337 <necoarcf6cksplanet@gmail.com>
2023-05-20 13:39:58 +0300

    Мега прототип главного экрана Надо сделать еще: -Добавление итемов по кнопке -Сортировка по активен не активен -Сделать закругленные края у итемов и разграничить их между собой

--- app/src/main/java/com/example/todo_list/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.todo_list
 
+import android.icu.number.Precision.integer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,11 +17,15 @@ class MainActivity : AppCompatActivity() {
         // Инициализируем список под названием events типа RecycleView, заполняя его с помощью адаптера EventsAdapter
         val events = findViewById<View>(R.id.mainList) as RecyclerView
         val eventsList = ArrayList<Event>()
-        for (i in 1..20) {
-            eventsList.add(Event("Событие №$i"))
+        var plus = findViewById<Button>(R.id.Plus)
+        var repeat = 0
+        plus.setOnClickListener{repeat++} //пытался сделать чтоб по кнопке добавлялось больше элементов ( не работает)
+        for (i in 1.. 20) {
+            eventsList.add(Event(""))
         }
         val adapter = EventsAdapter(eventsList)
         events.adapter = adapter
         events.layoutManager = LinearLayoutManager(this)
     }
 }
+
